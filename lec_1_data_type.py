3475e4a78c5b490bff79d27509ba937e8ae05efe
Daniillkn <114596866+Daniillkn@users.noreply.github.com>
2022-10-05 15:21:05 +0000

    a

---
+++ lec_1_data_type.py
@@ -0,0 +1,15 @@
+print(type(3))
+
+print(type(3.4))
+
+print(type('Молодец'))
+
+a = 'Hell'
+
+print(type(a))
+
+a = ['Help',1]
+print(type(a))
+print(type(True))
+print(type(False))
+print(type(None))
